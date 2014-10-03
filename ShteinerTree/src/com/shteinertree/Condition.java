cf6a31cc1e20aa80c427b8b5d0b22695b1f40add
k.velkerr@gmail.com <k.velkerr@gmail.com@ebe034fe-82b6-6e36-4651-e898510251b3>
2014-10-03 08:38:24 +0000

    Done!

--- ShteinerTree/src/com/shteinertree/Condition.java
+++ ShteinerTree/src/com/shteinertree/Condition.java
@@ -1,90 +1,181 @@
 
 package com.shteinertree;
 
-import com.shteinertree.exceptions.GraphInNotConnectedException;
 import com.shteinertree.utils.Utils;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import org.omg.CORBA.INTERNAL;
+import java.util.TreeSet;
 
 /**
  *
  * @author Ivchenko Oleg (Kirius VeLKerr)
  */
 public class Condition {
-    private int[][] matrix;
-//    private final Set<Integer> reqVert;
-    private final int[] reqVert;
+    private int[][] initialMatrix;
+    private int[][] treeMatrix;
+    private final TreeSet<Integer> reqVert;
+    private int approcsymWeight;
 
     public Condition(int[][] matrix, Set<Integer> reqVert) {
-        this.matrix = matrix;
-        this.reqVert = Utils.MathUtils.toIntMas(reqVert.toArray());
+        this.initialMatrix = matrix;
+        this.reqVert = new TreeSet<>(reqVert);
     }
     
     public void floidWarshall(){
-        for(int k=0; k<matrix.length; k++){
-            for(int i=0; i<matrix.length; i++){
-                for(int j=0; j<matrix.length; j++){
-                    if(matrix[i][k] != 0 && matrix[k][j] != 0)
-                    matrix[i][j] = Math.min(matrix[i][j], matrix[i][k] + matrix[k][j]);
+        for(int k=0; k<initialMatrix.length; k++){
+            for(int i=0; i<initialMatrix.length; i++){
+                for(int j=0; j<initialMatrix.length; j++){
+                    if(initialMatrix[i][k] != Integer.MAX_VALUE && 
+                       initialMatrix[k][j] != Integer.MAX_VALUE &&
+                       i != j && i != k && j != k &&
+                       initialMatrix[i][j] != Integer.MAX_VALUE)
+                    initialMatrix[i][j] = Math.min(initialMatrix[i][j], initialMatrix[i][k] + initialMatrix[k][j]);
                 }
             }
         }
     }
     
-    public int[] prim() throws GraphInNotConnectedException{
-        boolean[] used = new boolean[matrix.length];
-        int[] minEdge = new int[matrix.length];
-        int[] endOfTheEdge = new int[matrix.length];
-        Arrays.fill(minEdge, Integer.MIN_VALUE);
-        Arrays.fill(endOfTheEdge, -1);
-        minEdge[reqVert[0]] = 0;
-        for(int i=0; i<reqVert.length; ++i){
-            int newVertice = -1;
-            for(int verticeNum: reqVert){
-                if(!used[verticeNum] && (newVertice == -1 || minEdge[verticeNum] < minEdge[newVertice])){
-                    newVertice = verticeNum;
-                }
+    public void primKruscall(){
+        treeMatrix = new int[initialMatrix.length][initialMatrix.length];
+        fillMatrix(treeMatrix);
+        Set<Integer> usedIndexes = new HashSet<>();
+        boolean isFirstStep = true;
+        while(usedIndexes.size() != initialMatrix.length && !isEmpty(initialMatrix)){
+            int[] elem = findMin(usedIndexes);
+            treeMatrix[elem[0]][elem[1]] = elem[2];
+            usedIndexes.add(elem[0]);
+            deleteRow(elem[0]);
+            if(isFirstStep){
+                usedIndexes.add(elem[1]);
+                deleteRow(elem[1]);
+                isFirstStep = false;
             }
-            if(minEdge[newVertice] == Integer.MIN_VALUE){
-                throw new GraphInNotConnectedException();
-            }
-            used[newVertice] = true;
-            for(int verticeNum: reqVert){
-                if(verticeNum != newVertice && matrix[newVertice][verticeNum] < minEdge[verticeNum]){
-                    minEdge[verticeNum] = matrix[newVertice][verticeNum];
-                    endOfTheEdge[verticeNum] = newVertice;
+        }
+        countApprocsym();
+        symmMatr();
+    }
+    
+    private void deleteRow(int colIndex){
+        Arrays.fill(initialMatrix[colIndex], Integer.MAX_VALUE);
+    }
+    //i, j, value
+    private int[] findMin(Set<Integer> userIndexes){
+        int[] res = new int[3];
+        Arrays.fill(res, Integer.MAX_VALUE);
+        for(int i=0; i<initialMatrix.length; i++){
+            for(int j=0; j<initialMatrix.length; j++){
+                if((userIndexes.contains(j) || userIndexes.isEmpty()) && initialMatrix[i][j] < res[2]){
+                    res = new int[]{i, j, initialMatrix[i][j]};
                 }
             }
         }
-        return minEdge;
+        return res;
     }
     
-    public boolean check(){
-        return reqVert.length <= matrix.length && matrix[0].length == matrix.length;
+    private static void fillMatrix(int[][] matrix){
+        for(int[] row: matrix){
+            Arrays.fill(row, Integer.MAX_VALUE);
+        }
+    }
+    
+    private void countApprocsym(){
+        for (int[] treeMatrixRow : treeMatrix) {
+            for (int j = 0; j<treeMatrix.length; j++) {
+                int el = treeMatrixRow[j];
+                if(el != Integer.MAX_VALUE){
+                    approcsymWeight += el;
+                }
+            }
+        }
+    }
+    
+    public void deleteOptionalVertex(){
+        int shift = 0;
+        int initLen = initialMatrix.length;
+        for(int i=0; i<initLen; i++){
+            if(!reqVert.contains(i)){
+                strikeOut(i - shift);
+                shift++;
+            }
+        }
+    }
+    
+    private void strikeOut(int index){
+        int[][] res = new int[initialMatrix.length - 1][initialMatrix.length - 1];
+        for(int i=0; i<initialMatrix.length; i++){
+            for(int j=0; j<initialMatrix.length; j++){
+                if(i != index && j != index){
+                    int newI = i;
+                    int newJ = j;
+                    if(i > index){
+                        newI = i - 1;
+                    }
+                    if(j > index){
+                        newJ = j - 1;
+                    }
+                    res[newI][newJ] = initialMatrix[i][j];
+                }
+            }
+        }
+        initialMatrix = res;
+    }
+    
+    private void symmMatr(){
+        for(int i=0; i<treeMatrix.length; i++){
+            for(int j=0; j<treeMatrix.length; j++){
+                if(treeMatrix[i][j] != Integer.MAX_VALUE){
+                    treeMatrix[j][i] = treeMatrix[i][j];
+                }
+            }
+        }
+    }
+    
+    protected boolean check(){
+        return reqVert.size() <= initialMatrix.length && initialMatrix[0].length == initialMatrix.length;
     }
     
     @Override
     public String toString(){
         StringBuilder sb = new StringBuilder();
-        Utils.ToString.matrixToString(matrix, sb);
+        if(!isEmpty(initialMatrix)){
+            Utils.ToString.matrixToString(initialMatrix, sb);
+        }
+        else{
+            Utils.ToString.matrixToString(treeMatrix, sb);
+        }
         sb.append("REQ vertices: ");
-        Utils.ToString.masToString(reqVert, sb);
+        Utils.ToString.collectionToString(reqVert.iterator(), sb);
         return sb.toString();
     }
     
+    private static boolean isEmpty(int[][] matrix){
+        for (int[] matrixRow : matrix) {
+            for (int el: matrixRow) {
+                if(el != Integer.MAX_VALUE){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    @Deprecated
     public List<Edge> toEdgesList(){
         List<Edge> edges = new ArrayList<>();
-        for(int i=0; i<matrix.length; i++){
-            for(int j=0; j<matrix.length; j++){
-                if(j >= i && matrix[i][j] != 0 && matrix[i][j] != Integer.MAX_VALUE){
-                    edges.add(new Edge(i, j, matrix[i][j]));
+        for(int i=0; i<treeMatrix.length; i++){
+            for(int j=0; j<treeMatrix.length; j++){
+                if(treeMatrix[i][j] != Integer.MAX_VALUE){
+                    edges.add(new Edge(i, j, treeMatrix[i][j]));
                 }
             }
         }
         return edges;
+    }
+
+    public int getApprocsymWeight() {
+        return approcsymWeight;
     }
 }
--- ShteinerTree/src/com/shteinertree/ShteinerTree.java
+++ ShteinerTree/src/com/shteinertree/ShteinerTree.java
@@ -3,9 +3,9 @@ package com.shteinertree;
 import com.shteinertree.exceptions.GraphInNotConnectedException;
 import com.shteinertree.generator.ConditionGenerator;
 import com.shteinertree.utils.Utils;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Collections;
-import java.util.List;
+import java.io.InputStreamReader;
 
 /**
  *
@@ -13,26 +13,47 @@ import java.util.List;
  */
 public class ShteinerTree {
     private static final int MATRIX_SIZE = 10;
-    private static final String fname = "1";
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException, GraphInNotConnectedException {
-        ConditionGenerator cg = ConditionGenerator.getInstance();
-        cg.generate(MATRIX_SIZE);
-        Condition cond = cg.getCondition();
-//        Condition cond = Utils.Input.readFromTextFile(fname);
-//        System.out.println(cond.toString() + "\n");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int menu = 0;
+        while(menu == 0){
+            String incorrectStr = "\tIncorrect number!";
+            System.out.println("If you want to read graph from file, press 1.");
+            System.out.print("If you want to generate its, press 2: ");
+            try{
+                menu = Integer.parseInt(br.readLine());
+                if(menu != 1 && menu != 2){
+                    menu = 0;
+                    System.out.println(incorrectStr);
+                }
+            }
+            catch(NumberFormatException nfe){
+                System.out.println(incorrectStr);
+            }
+        }
+        Condition cond = null;
+        if(menu == 1){
+            System.out.print("\tInput filename (without file extension): ");
+            String fname = br.readLine();
+            cond = Utils.Input.readFromTextFile(fname);
+        }
+        else{
+            ConditionGenerator cg = ConditionGenerator.getInstance();
+            cg.generate(MATRIX_SIZE);
+            cond = cg.getCondition();
+        }
         System.out.println(cond.toString() + "\n");
         cond.floidWarshall();
         System.out.println(cond.toString() + "\n");
-        List<Edge> edges = cond.toEdgesList();
-        Collections.sort(edges);
-        for(Edge ed: edges){
-            System.out.println(ed.toString());
-        }
-        //System.out.println(Utils.ToString.masToString(cond.prim(), new StringBuilder()));
+        cond.deleteOptionalVertex();
+        System.out.println(cond.toString() + "\n");
+        cond.primKruscall();
+        System.out.println(cond.toString() + "\n");
+        System.out.println("Tree's weight: " + cond.getApprocsymWeight());
     }
     
 }
